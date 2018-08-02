767f5651a8c1668a12e2fa9428d7f2a1520b3abd
Nell David Soledad <nell@sym.ph>
2018-08-02 15:35:30 +0800

    Completed issue form and list from welcome page

---
+++ src/scenes/Issue/components/Card.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+  state = {
+    description: '',
+    address: '',
+    file: null,
+    reporter: 'Issue page form',
+    status: 'New'
+  }
+  change = e => {
+    let state = {};
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  }
+  submit = e => {
+    e.preventDefault();
+    let form = new FormData();
+    form.append('description', this.state.description);
+    form.append('address', this.state.address);
+    form.append('reporter', this.state.reporter);
+    form.append('status', this.state.status);
+    form.append('attachments', this.state.file);
+    fetch(`/api/issues/`, {
+      method: 'post',
+      mode: 'cors',
+      body: form
+    })
+    .then(response => response.json())
+    .then(issue => {
+      console.log(issue);
+      this.props.closeNewIssueForm();
+    });
+  }
+  selectFile = e => {
+    let file = document.getElementById('attachment');
+    file.click();
+  }
+  fileSelected = e => {
+    let file = e.target.files[0];
+    let button = e.target.parentNode.firstChild;
+    let span = button.nextSibling;
+    let reader = new FileReader();
+    reader.onload = e => {
+      button.style.backgroundImage = `url(${e.target.result})`;
+      span.innerHTML = file.name;
+      this.setState({
+        file: file
+      });
+    }
+    reader.readAsDataURL(file);
+  }
+  render() {
+    let filename = '';
+    if(this.props.issue && this.props.issue.attachments) {
+      filename = this.props.issue.attachments.split(/[\s/]+/);
+      filename = filename[filename.length-1];
+      console.log(filename);
+    }
+    return this.props.editable ? (
+      <form className="clearfix" onSubmit={this.submit}>
+        <div className="issue p-3 my-3">
+            <h2>
+              <input type="text" placeholder="Issue description" name="description" onChange={this.change} value={this.state.description} />
+            </h2>
+            <div className="d-flex">
+              <div>Address:</div>
+              <div className="ml-2 flex-fill">
+                <input type="text" placeholder="Address of the issue" name="address" onChange={this.change} value={this.state.address} />
+              </div>
+            </div>
+          <div className="attachments mt-3 p-3">
+            <button type="button" className="rounded-circle mx-3 attachment-plus text-white" onClick={this.selectFile}>+</button>
+            <span>Add picture</span>
+            <input type="file" name="attachment" id="attachment" className="d-none" onChange={this.fileSelected} />
+          </div>
+        </div>
+        <div className="d-flex flex-row float-right">
+          <button type="submit" className="btn btn-link">Send to us</button>
+          <button type="button" className="btn btn-link" onClick={this.props.closeNewIssueForm}>Discard</button>
+        </div>
+      </form>
+    ) : (
+      <div className="issue p-3 my-3">
+        <h2>{this.props.issue.description}</h2>
+        <span>Address: {this.props.issue.address}</span>
+        <div className="attachments mt-3 p-3">
+          <img src={this.props.issue.attachments} alt="Attachment" className="rounded-circle mx-3"/>
+          <a href={this.props.issue.attachments}>
+            <span>{filename}</span>
+          </a>
+        </div>
+      </div>
+    )
+  }
+}
--- src/scenes/Issue/index.js
+++ src/scenes/Issue/index.js
@@ -1,74 +1,52 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import Card from './components/Card';
 import './Issue.css';
-
-class Card extends Component {
-  state = {
-    description: '',
-    address: ''
-  }
-  submit = (e) => {
-    console.log(e);
-  }
-  render() {
-    return this.props.editable ? (
-      <div className="issue p-3 my-3">
-          <h2><input type="text" placeholder="Issue description" name="description" /></h2>
-          <div className="d-flex">
-            <div>Address:</div>
-            <div className="ml-2 flex-fill">
-              <input type="text" placeholder="Address of the issue" name="address" />
-            </div>
-          </div>
-        <div className="attachments mt-3 p-3">
-          <button className="rounded-circle mx-3 attachment-plus text-white">+</button>
-            <span>Add picture</span>
-        </div>
-        <div className="d-flex flex-row float-right">
-          <button className="btn btn-link">Send to us</button>
-          <button className="btn btn-link">Discard</button>
-        </div>
-      </div>
-    ) : (
-      <div className="issue p-3 my-3">
-        <h2>There is a hole near my home</h2>
-        <span>Address: Jahu tänav 1-15</span>
-        <div className="attachments mt-3 p-3">
-          <img src="https://picsum.photos/30/30" alt="Attachment" className="rounded-circle mx-3"/>
-          <span>Picture of the hole.jpg</span>
-        </div>
-      </div>
-    )
-  }
-}
 
 export default class Issue extends Component {
   state = {
     currentDate: moment().format('YYYY-MM-DD'),
+    issues: [],
     newIssue: false
   }
   componentDidMount() {
     document.title = 'Report an Issue'
+    fetch('/api/issues/')
+    .then(response => response.json())
+    .then(issues => {
+      console.log(issues);
+      this.setState({
+        issues: issues.concat(issues)
+      });
+    });
   }
-  addNewIssue = (e) => {
+  openNewIssueForm = (e) => {
     this.setState({
       newIssue: true
+    });
+  }
+  closeNewIssueForm = (e) => {
+    this.setState({
+      newIssue: false
     });
   }
   render() {
     return (
       <div className="issues-list m-3">
         <h1>Issue {this.state.currentDate}</h1>
-        <Card />
-        { this.state.newIssue && (
-          <Card editable />
-        )}
-        { !this.state.newIssue && (
-          <div className="attachments mt-3 p-3">
-            <button className="rounded-circle mx-3 attachment-plus text-white" onClick={this.addNewIssue}>+</button>
-            <span>Add new aspect</span>
-          </div>
-        )}
+        {this.state.issues.map(issue => (
+          <Card issue={issue} />
+        ))}
+        {
+          this.state.newIssue
+          ? <Card editable closeNewIssueForm={this.closeNewIssueForm} />
+          : (
+            <div className="attachments mt-3 p-3">
+              <button className="rounded-circle mx-3 attachment-plus text-white" onClick={this.openNewIssueForm}>+</button>
+              <span>Add new aspect</span>
+            </div>
+          )
+        }
       </div>
     )
   }
