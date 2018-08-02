df824fa4b615392de53653515addf925773a8cc3
Nell David Soledad <nell@sym.ph>
2018-08-02 12:34:40 +0800

    Partial issue

--- src/scenes/Issue/index.js
+++ src/scenes/Issue/index.js
@@ -2,37 +2,45 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import './Issue.css';
 
-const Card = ({ editable }) => (
-    <div className="issue p-3 my-3">
-      {
-        editable
-        ? <h2><input type="text" placeholder="Issue description" name="description" /></h2>
-        : <h2>There is a hole near my home</h2>
-      }
-      {
-        editable ? (
+class Card extends Component {
+  state = {
+    description: '',
+    address: ''
+  }
+  submit = (e) => {
+    console.log(e);
+  }
+  render() {
+    return this.props.editable ? (
+      <div className="issue p-3 my-3">
+          <h2><input type="text" placeholder="Issue description" name="description" /></h2>
           <div className="d-flex">
             <div>Address:</div>
             <div className="ml-2 flex-fill">
               <input type="text" placeholder="Address of the issue" name="address" />
             </div>
           </div>
-        ) : <span>Address: Jahu tänav 1-15</span>
-      }
-      <div className="attachments mt-3 p-3">
-        {
-          editable
-          ? <button className="rounded-circle mx-3 attachment-plus text-white">+</button>
-          : <img src="https://picsum.photos/30/30" alt="Attachment" className="rounded-circle mx-3"/>
-        }
-        {
-          editable
-          ? <span>Add picture</span>
-          : <span>Picture of the hole.jpg</span>
-        }
+        <div className="attachments mt-3 p-3">
+          <button className="rounded-circle mx-3 attachment-plus text-white">+</button>
+            <span>Add picture</span>
+        </div>
+        <div className="d-flex flex-row float-right">
+          <button className="btn btn-link">Send to us</button>
+          <button className="btn btn-link">Discard</button>
+        </div>
       </div>
-    </div>
-  );
+    ) : (
+      <div className="issue p-3 my-3">
+        <h2>There is a hole near my home</h2>
+        <span>Address: Jahu tänav 1-15</span>
+        <div className="attachments mt-3 p-3">
+          <img src="https://picsum.photos/30/30" alt="Attachment" className="rounded-circle mx-3"/>
+          <span>Picture of the hole.jpg</span>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default class Issue extends Component {
   state = {
@@ -59,12 +67,6 @@ export default class Issue extends Component {
           <div className="attachments mt-3 p-3">
             <button className="rounded-circle mx-3 attachment-plus text-white" onClick={this.addNewIssue}>+</button>
             <span>Add new aspect</span>
-          </div>
-        )}
-        { this.state.newIssue && (
-          <div className="d-flex flex-row float-right">
-            <button className="btn btn-link">Send to us</button>
-            <button className="btn btn-link">Discard</button>
           </div>
         )}
       </div>
