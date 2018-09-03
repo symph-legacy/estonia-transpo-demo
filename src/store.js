12107d632eaef59fdd63596fb10b5e26fbb04ef7
John Rey Evangelista <johnrey@John-Reys-Macbook-Pro.local>
2018-09-03 14:25:34 +0800

    make migrations

--- src/store.js
+++ src/store.js
@@ -1,21 +1,22 @@
 import { createBrowserHistory } from 'history'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
-import { composeWithDevTools } from 'redux-devtools-extension'
+// import { composeWithDevTools } from 'redux-devtools-extension'
 
 
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
-// const initialState = {};
+const initialState = {};
 const middleware = [thunk];
 
 export const history = createBrowserHistory();
 
 const store = createStore(
     connectRouter(history)(rootReducer),
-    /* initialState, */
-    composeWithDevTools(
+    initialState,
+    // composeWithDevTools(
+    compose(
         applyMiddleware(
             routerMiddleware(history),
             ...middleware
