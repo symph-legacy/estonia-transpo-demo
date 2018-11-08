065fc1c79495a9f132998eb22aa40bcdc13db9dd
John Rey Evangelista <johnrey@John-Reys-Macbook-Pro.local>
2018-11-08 21:52:44 +0800

    modify settings.py

--- estoniatranspo/settings.py
+++ estoniatranspo/settings.py
@@ -75,9 +75,6 @@ MIDDLEWARE = [
 LOGIN_REDIRECT_URL = "/"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework.authentication.TokenAuthentication',
-    ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     )
--- estoniatranspo/urls.py
+++ estoniatranspo/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken import views as rest_framework_views
-
 from estoniatranspo.app import views
 
 class OptionalSlashRouter(DefaultRouter):
